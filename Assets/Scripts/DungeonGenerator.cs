b0fe8770027af8a38b1fa54163ff6ee9914e15bf
DamianKoch1 <damian.koch.001@gmail.com>
2020-05-13 18:36:54 +0200

    added semi working procedural dungeons, connectors missing sometimes

---
+++ Assets/Scripts/DungeonGenerator.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DungeonGenerator : MonoBehaviour
+{
+    private static DungeonGenerator instance;
+
+    public static DungeonGenerator Instance
+    {
+        get
+        {
+            if (!instance)
+            {
+                instance = FindObjectOfType<DungeonGenerator>();
+            }
+            return instance;
+        }
+    }
+
+    [SerializeField]
+    private Tile groundTile;
+
+    public static Tile Ground => Instance.groundTile;
+
+    [SerializeField]
+    private Tile wallTile;
+
+    public static Tile Wall => Instance.wallTile;
+
+    public Tilemap tilemap;
+
+    [SerializeField, Range(3, 20)]
+    private int roomHalfSize;
+
+    [SerializeField, Range(0, 10)]
+    private int connectorHalfLength;
+
+    [SerializeField, Range(2, 10)]
+    private int connectorHalfWidth;
+
+
+    [SerializeField, Range(0, 10)]
+    private int maxDepth;
+
+    [SerializeField, Range(0, 1)]
+    private float roomExitChance = 0.8f;
+
+    public DungeonRoom startRoom;
+
+    private Dictionary<Vector2Int, DungeonRoom> rooms;
+    private List<DungeonRoom> roomsToExpandAt;
+
+    public List<DungeonRoom> roomList;
+
+    [ContextMenu(" ")]
+    public void Generate()
+    {
+        rooms = new Dictionary<Vector2Int, DungeonRoom>();
+        roomList = new List<DungeonRoom>();
+        roomsToExpandAt = new List<DungeonRoom>();
+        tilemap.ClearAllTiles();
+        startRoom = new DungeonRoom(roomHalfSize, roomExitChance, 0, 0, Vector2.zero);
+        roomList.Add(startRoom);
+        RegisterRoom(startRoom);
+        int maxGenerationTries = 500;
+        while (maxGenerationTries > 0 && roomsToExpandAt.Count > 0)
+        {
+            ExpandFromRoom(roomsToExpandAt[0]);
+            roomsToExpandAt.RemoveAt(0);
+            maxGenerationTries--;
+        }
+
+        foreach (var room in rooms.Values)
+        {
+            room.AddTiles();
+        }
+    }
+
+    private void RegisterRoom(DungeonRoom room)
+    {
+        rooms[room.idx] = room;
+        roomsToExpandAt.Add(room);
+    }
+
+    private void ExpandFromRoom(DungeonRoom room)
+    {
+        ExpandToDir(room, Vector2Int.up);
+        ExpandToDir(room, Vector2Int.down);
+        ExpandToDir(room, Vector2Int.left);
+        ExpandToDir(room, Vector2Int.right);
+    }
+
+    private void ExpandToDir(DungeonRoom room, Vector2Int dir)
+    {
+        if (Mathf.Abs(room.idx.x + dir.x) > maxDepth) return;
+        if (Mathf.Abs(room.idx.y + dir.y) > maxDepth) return;
+
+        if (rooms.ContainsKey(room.idx + dir)) return;
+        if (!room.exits[dir]) return;
+        var newRoom = room.AddNeighbour(dir, roomHalfSize, connectorHalfWidth, connectorHalfLength, roomExitChance);
+        newRoom.exits[dir * -1] = true;
+        RegisterRoom(newRoom);
+        roomList.Add(newRoom);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(transform.position, new Vector3(roomHalfSize * 2, roomHalfSize * 2, 0));
+        Gizmos.DrawWireCube(transform.position + Vector3.up * (roomHalfSize + connectorHalfLength), new Vector3(connectorHalfWidth * 2, connectorHalfLength * 2, 0));
+    }
+}
+
---
+++ Assets/Scripts/DungeonRoom.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[System.Serializable]
+public class DungeonRoom
+{
+    public Vector2Int halfSize;
+
+    public Dictionary<Vector2Int, bool> exits;
+
+    public Vector2Int idx;
+    public Vector2 position;
+
+
+    public void AddTiles()
+    {
+        var centerTilePos = DungeonGenerator.Instance.tilemap.WorldToCell(position);
+        for (int x = -halfSize.x; x < halfSize.x; x++)
+        {
+            for (int y = -halfSize.y; y < halfSize.y; y++)
+            {
+                Tile tileToPlace = DungeonGenerator.Ground;
+                if (x == -halfSize.x)
+                {
+                    if (!exits[Vector2Int.left])
+                    {
+                        tileToPlace = DungeonGenerator.Wall;
+                    }
+                    else if (y > 0 || y < -1)
+                    {
+                        tileToPlace = DungeonGenerator.Wall;
+                    }
+                }
+                else if (x == halfSize.x - 1)
+                {
+                    if (!exits[Vector2Int.right])
+                    {
+                        tileToPlace = DungeonGenerator.Wall;
+                    }
+                    else if (y > 0 || y < -1)
+                    {
+                        tileToPlace = DungeonGenerator.Wall;
+                    }
+                }
+                else if (y == -halfSize.y)
+                {
+                    if (!exits[Vector2Int.down])
+                    {
+                        tileToPlace = DungeonGenerator.Wall;
+                    }
+                    else if (x > 0 || x < -1)
+                    {
+                        tileToPlace = DungeonGenerator.Wall;
+                    }
+                }
+                else if (y == halfSize.y - 1)
+                {
+                    if (!exits[Vector2Int.up])
+                    {
+                        tileToPlace = DungeonGenerator.Wall;
+                    }
+                    else if (x > 0 || x < -1)
+                    {
+                        tileToPlace = DungeonGenerator.Wall;
+                    }
+                }
+                DungeonGenerator.Instance.tilemap.SetTile(new Vector3Int((int)position.x + x, (int)position.y + y, 0), tileToPlace);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Use this for rooms
+    /// </summary>
+    /// <param name="_halfSize"></param>
+    /// <param name="exitChance"></param>
+    public DungeonRoom(int _halfSize, float exitChance, int x, int y, Vector2 _position)
+    {
+        exits = new Dictionary<Vector2Int, bool>();
+        halfSize = new Vector2Int(_halfSize, _halfSize);
+        exits[Vector2Int.up] = Random.Range(0f, 1f) < exitChance;
+        exits[Vector2Int.down] = Random.Range(0f, 1f) < exitChance;
+        exits[Vector2Int.right] = Random.Range(0f, 1f) < exitChance;
+        exits[Vector2Int.left] = Random.Range(0f, 1f) < exitChance;
+        idx = new Vector2Int(x, y);
+        position = _position;
+    }
+
+    /// <summary>
+    /// Use this for connectors
+    /// </summary>
+    /// <param name="halfWidth"></param>
+    /// <param name="halfLength"></param>
+    /// <param name="vertical"></param>
+    public DungeonRoom(int halfWidth, int halfLength, Vector2 _position, bool vertical)
+    {
+        exits = new Dictionary<Vector2Int, bool>();
+        if (vertical)
+        {
+            halfSize = new Vector2Int(halfWidth, halfLength);
+        }
+        else halfSize = new Vector2Int(halfLength, halfWidth);
+        exits[Vector2Int.up] = vertical;
+        exits[Vector2Int.down] = vertical;
+        exits[Vector2Int.right] = !vertical;
+        exits[Vector2Int.left] = !vertical;
+        position = _position;
+    }
+
+    public DungeonRoom AddNeighbour(Vector2Int dir, int roomHalfSize, int connectorHalfWidth, int connectorHalfLength, float roomExitChance)
+    {
+        bool vertical = dir.y != 0;
+        var roomPos = position + dir * (roomHalfSize + connectorHalfLength);
+        var connector = new DungeonRoom(connectorHalfWidth, connectorHalfLength, roomPos, vertical);
+        connector.AddTiles();
+        roomPos += dir * (roomHalfSize + connectorHalfLength);
+        return new DungeonRoom(roomHalfSize, roomExitChance, idx.x + dir.x, idx.y + dir.y, roomPos);
+    }
+}
